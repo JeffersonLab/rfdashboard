6ded9af8f3e41b3a33c0c23f66d095478edc0b4d
Adam Carpenter <adamc@jlab.org>
2017-07-27 10:07:49 -0400

    Added nexDay method and comments

--- src/java/org/jlab/rfd/business/util/DateUtil.java
+++ src/java/org/jlab/rfd/business/util/DateUtil.java
@@ -19,13 +19,25 @@ import java.util.Date;
 public class DateUtil {
     private static final SimpleDateFormat YMD_DATE_FORMATTER = new SimpleDateFormat("yyyy-MM-dd");
     
+    // Converts dates to day only.
     public static Date truncateToDays(Date d1) throws ParseException{
         return YMD_DATE_FORMATTER.parse(YMD_DATE_FORMATTER.format(d1));
     }
     
+    // Returns a date object that represents 12AM of the very next day
+    public static Date nextDay(Date d1) throws ParseException {
+       Date curr = truncateToDays(d1);
+       Calendar cal = Calendar.getInstance();
+       cal.setTime(curr);
+       cal.add(Calendar.DATE, 1);
+       return cal.getTime();
+    }
+    
+    // Returns the number of whole days between to points in time
     public static long getDifferenceInDays(Date d1, Date d2) {
 
-        // Calculate the naive difference in days without consider DST, leap seconds, etc.
+        // Calculate the naive difference in days without consider DST, leap seconds, etc..  This gets you close without having
+        // to loop a bunch for bigger differences
         long t1 = d1.getTime();
         long t2 = d2.getTime();
         long diffDays = (t2 - t1) / (1000L * 60 * 60 * 24);
