e21fdf379588adae5bc57b5fbd9e9a05ac86bc7f
Adam Carpenter <adamc@jlab.org>
2017-07-27 14:16:07 -0400

    Initial commit

---
+++ src/java/org/jlab/rfd/model/TimeUnit.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jlab.rfd.model;
+
+/**
+ * A representation of common time units used by controller classes, etc.
+ * @author adamc
+ */
+public enum TimeUnit {
+    DAY, WEEK
+}
