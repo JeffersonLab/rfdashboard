469502d3176c0f450c95278d154248cc1106cced
Adam Carpenter <adamc@jlab.org>
2017-07-29 21:26:53 -0400

    Added ModAnodeHarvester color set

--- web/resources/js/utils.js
+++ web/resources/js/utils.js
@@ -65,6 +65,8 @@ jlab.colors = jlab.colors || {};
 jlab.colors.linacs = ["#FF0000", "#006400", "#273BE7", "#333333", "#ECAF2F"];
 // C25, C50, C100, Total, Unknown
 jlab.colors.cmtypes = ["#FFCE00", "#0375B4", "#007849", "#333333", "#ECAF2F"];
+// Total 1050, Total 1050 No M.A.V, Total 1090, Total 1090 No M.A.V
+jlab.colors.modAnodeHarvester = ["#B22222", "#483D8B", "#FF0000", "#0000FF"];
 
 // This assumes that you are passing an array of flot data series (a 3d array in all).  It returns the smallest difference
 // between any two points in a single series.
