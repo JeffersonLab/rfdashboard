25847ebce528b98b4842b29c9ad28e6e57438ce2
Adam Carpenter <adamc@jlab.org>
2017-08-28 13:09:29 -0400

    Updated to use flot-charts.js

--- web/resources/js/bypassed.js
+++ web/resources/js/bypassed.js
@@ -6,67 +6,54 @@
 
 var jlab = jlab || {};
 
-jlab.bypassedUrl = "/RFDashboard/ajax/bypassed";
 
 jlab.bypassed = jlab.bypassed || {};
-jlab.bypassed.loadCharts = function (url, start, end, timeUnit) {
+jlab.bypassed.ajaxUrl = "/RFDashboard/ajax/bypassed";
+jlab.bypassed.bypassedUrl = "/RFDashboard/bypassed";
 
-    var settings1 = {
-        chartId: 'bypassed-count-by-linac',
-        url: url,
-        start: start,
-        end: end,
-        timeUnit: timeUnit,
-        colors: jlab.colors.linacs,
-        yLabel: "# Cavities Bypassed",
-        title: "Bypassed Cavities<br>(By Linac)",
-        timeMode: true,
-        ajaxData: {
-            "start": start,
-            "end": end,
-            "factor": "linac",
-            "timeUnit": timeUnit
-        }
-    };
-    jlab.barChart.updateChart(settings1);
-    $('#bypassed-count-by-linac').bind("plotclick", function (event, pos, item) {
-        if (item) {
-            var timestamp = item.series.data[item.dataIndex][0];
-            var dateString = jlab.millisToDate(timestamp);
-            var url = "/RFDashboard/bypassed?start=" + jlab.start + "&end=" + jlab.end + "&tableDate=" + dateString;
-            console.log("Linking to " + url);
-            window.location.href = url;
-        }
-    });
 
-    var settings2 = {
-        chartId: 'bypassed-count-by-cmtype',
-        url: url,
-        start: start,
-        end: end,
-        timeUnit: timeUnit,
-        colors: jlab.colors.cmtypes,
-        yLabel: "# Cavities Bypassed",
-        timeMode: true,
-        title: "Bypassed Cavities<br>(By Module Type)",
-        ajaxData: {
-            "start": start,
-            "end": end,
-            "factor": "cmtype",
-            "timeUnit": timeUnit
+jlab.bypassed.loadBypassedCountByFactorChart =  function(chartId, start, end, url, timeUnit, factor) {
+     jlab.showChartLoading(chartId);
+    var lemScan = $.getJSON(url, {start: start, end: end, out: "flot", timeUnit: timeUnit, factor: factor});
+    lemScan.done(function(json) {
+        var settings = {
+            colors: jlab.colors.cmtypes,          // Grab the North, South, and Total colors
+            labels: json.labels,
+            timeUnit: timeUnit,
+            title: "<strong>Bypassed Cavities<br>(by " + factor + ")</strong>",
+            tooltips: true,
+            tooltipX: "Date",
+            tooltipY: "Value",
+            legend: true,
+            chartType: "bar"
+        };
+        var flotOptions = {
+            xaxis: { mode: "time" },
+            yaxis: { axisLabel: "# Bypassed Cavities"},
+            grid: { clickable: true }
+        };
+        
+        var flotData = [];
+        for(i=0; i < json.data.length; i++) {
+            flotData[i] = {data: json.data[i], points:{show: false} };
         }
-    };
-    jlab.barChart.updateChart(settings2);
-    $('#bypassed-count-by-cmtype').bind("plotclick", function (event, pos, item) {
-        if (item) {
-            var timestamp = item.series.data[item.dataIndex][0];
-            var dateString = jlab.millisToDate(timestamp);
-            var url = "/RFDashboard/bypassed?start=" + jlab.start + "&end=" + jlab.end + "&tableDate=" + dateString;
-            console.log("Linking to " + url);
-            window.location.href = url;
-        }
+
+        jlab.hideChartLoading(chartId);
+        var plot = jlab.flotCharts.drawChart(chartId, flotData, flotOptions, settings);
+        $("#" + chartId).bind("plotclick", function (event, pos, item) {
+            if (item) {
+                var timestamp = item.series.data[item.dataIndex][0];
+                var dateString = jlab.millisToDate(timestamp);
+                var url = jlab.bypassed.bypassedUrl + "?start=" + start + "&end=" + end + "&tableDate=" + dateString + "&timeUnit=" + timeUnit;
+                window.location.href = url;
+            }
+        });
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        jlab.hideChartLoading(chartId, "Error querying data");
+        console.log("Error querying data.\n  textStatus: " + textStatus + "\n  errorThrown: " + errorThrown);;
     });
 };
+
 
 /* 
  * Create a bypassed cavity data table.
@@ -80,7 +67,6 @@ jlab.bypassed.createTable = function (tableId, date) {
         dates: [date],
         timeUnit: "day",
         success: function (jsonData, textStatus, jqXHR) {
-            console.log(jsonData);
             var data = jsonData.data;
             var tableString = "<table class=\"tablesorter\">";
             tableString += "<thead><tr><th>Name</th><th>Module Type</th><th>Mod Anode Voltage (kV)</th><th>GSET</th></tr></thead>";
@@ -107,7 +93,8 @@ jlab.bypassed.createTable = function (tableId, date) {
 
 $(function () {
     jlab.bypassed.createTable("bypassed-table", jlab.tableDate);
-    jlab.bypassed.loadCharts(jlab.bypassedUrl, jlab.start, jlab.end, jlab.timeUnit);
+    jlab.bypassed.loadBypassedCountByFactorChart("bypassed-count-by-linac", jlab.start, jlab.end, jlab.bypassed.ajaxUrl, jlab.timeUnit, "linac");
+    jlab.bypassed.loadBypassedCountByFactorChart("bypassed-count-by-cmtype", jlab.start, jlab.end, jlab.bypassed.ajaxUrl, jlab.timeUnit, "cmtype");
 
     $(".date-field").datepicker({
         dateFormat: "yy-mm-dd"
