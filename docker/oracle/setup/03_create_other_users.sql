8412219deded0bb1bb61b479bb52efb2ec1a6cab
Adam Carpenter <adamc@jlab.org>
2023-03-09 09:25:52 -0500

    Added commands rfd_comments table

--- docker/oracle/setup/03_create_other_users.sql
+++ docker/oracle/setup/03_create_other_users.sql
@@ -11,11 +11,14 @@ GRANT CREATE SYNONYM TO rfgradteam_read;
 
 CREATE OR REPLACE SYNONYM rfgradteam_read.lem_scan
   FOR rfgradteam_owner.lem_scan;
+CREATE OR REPLACE SYNONYM rfgradteam_read.rfd_comments
+  FOR rfgradteam_owner.rfd_comments;
 
 -- Add additional reader permissions to for the new mod_anode_harvester_* tables
 GRANT SELECT ON rfgradteam_owner.mod_anode_harvester_scan TO rfgradteam_read;
 GRANT SELECT ON rfgradteam_owner.mod_anode_harvester_linac_scan TO rfgradteam_read;
 GRANT SELECT ON rfgradteam_owner.mod_anode_harvester_gset TO rfgradteam_read;
+GRANT SELECT ON rfgradteam_owner.rfd_comments TO rfgradteam_read;
 
 -- Add the synonyms so we can access these tables using the shortname as this user
 CREATE OR REPLACE SYNONYM rfgradteam_read.mod_anode_harvester_scan FOR rfgradteam_owner.mod_anode_harvester_scan;
@@ -36,6 +39,8 @@ CREATE OR REPLACE SYNONYM rfgradteam_rw.lem_scan
   FOR rfgradteam_owner.lem_scan;
 CREATE OR REPLACE SYNONYM rfgradteam_rw.lem_scan_seq
   FOR rfgradteam_owner.lem_scan_seq;
+CREATE OR REPLACE SYNONYM rfgradteam_rw.rfd_comments
+  FOR rfgradteam_owner.rfd_comments;
 
     -- Add additional read/write permissions to for the new mod_anode_harvester_* tables
 GRANT SELECT ON rfgradteam_owner.mod_anode_harvester_scan TO rfgradteam_rw;
@@ -44,6 +49,8 @@ GRANT SELECT ON rfgradteam_owner.mod_anode_harvester_linac_scan TO rfgradteam_rw
 GRANT INSERT ON rfgradteam_owner.mod_anode_harvester_linac_scan TO rfgradteam_rw;
 GRANT SELECT ON rfgradteam_owner.mod_anode_harvester_gset TO rfgradteam_rw;
 GRANT INSERT ON rfgradteam_owner.mod_anode_harvester_gset TO rfgradteam_rw;
+GRANT SELECT ON rfgradteam_owner.rfd_comments TO rfgradteam_rw;
+GRANT INSERT ON rfgradteam_owner.rfd_comments TO rfgradteam_rw;
 
 -- Add the synonyms so we can access these tables using the shortname as this user
 CREATE OR REPLACE SYNONYM rfgradteam_rw.mod_anode_harvester_scan FOR rfgradteam_owner.mod_anode_harvester_scan;
