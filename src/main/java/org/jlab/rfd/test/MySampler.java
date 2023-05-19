842f61ee7fa1a0089fc9d0b314c03558f45c8168
Adam Carpenter <adamc@jlab.org>
2023-05-19 14:49:47 -0400

    Added simple end points for integration testing

--- src/main/java/org/jlab/rfd/test/MySampler.java
+++ src/main/java/org/jlab/rfd/test/MySampler.java
@@ -10,7 +10,7 @@ import java.io.PrintWriter;
 public class MySampler extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String out = "{\"data\":[{\"date\":\"2018-01-01T00:00:00\",\"values\":[{\"R12XHTPLEM\":\"73.4588\"},{\"R13XHTPLEM\":\"86.6749\"}]}]}";
+        String out = "{\"channels\":{\"R12XHTPLEM\":{\"metadata\":{\"name\":\"R12XHTPLEM\",\"datatype\":\"DBR_DOUBLE\",\"datasize\":1,\"datahost\":\"opsmya8\",\"ioc\":\"iocnl1\",\"active\":true},\"data\":[{\"d\":\"2023-01-01T00:00:00\",\"v\":72.117897}],\"returnCount\":1},\"R13XHTPLEM\":{\"metadata\":{\"name\":\"R13XHTPLEM\",\"datatype\":\"DBR_DOUBLE\",\"datasize\":1,\"datahost\":\"opsmya13\",\"ioc\":\"iocnl1\",\"active\":true},\"data\":[{\"d\":\"2023-01-01T00:00:00\",\"v\":66.913399}],\"returnCount\":1}}}";
         response.setContentType("application/json");
         try(PrintWriter pw = response.getWriter()) {
             pw.println(out);
