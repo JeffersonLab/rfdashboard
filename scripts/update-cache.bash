fdc963a1501311491ed65f7120388dff31a62bac
Adam Carpenter <apcarp@users.noreply.github.com>
2023-08-31 08:02:24 -0400

    Update url and query length

--- scripts/update-cache.bash
+++ scripts/update-cache.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-# The RFDashboard caches cavity queries - need to update this every so often for acceptable performance
-wget "https://accweb.acc.jlab.org/RFDashboard/ajax/cavity?end=`date +'%F'`&start=`date --date='1 year ago' +'%F'`&timeUnit=day" -O /dev/null
-wget "https://accwebtest.acc.jlab.org/RFDashboard/ajax/cavity?end=`date +'%F'`&start=`date --date='1 year ago' +'%F'`&timeUnit=day" -O /dev/null
+# The RFDashboard caches cavity queries in a database as they take 10-20 seconds to complete.  Run this every morning to prime
+#  the cache for that day.  Querying one week gives a small margin for error in case the wget fails or is not run.
+wget --quiet "https://ace.jlab.org/RFDashboard/ajax/cavity?end=`date +'%F'`&start=`date --date='1 week ago' +'%F'`&timeUnit=day" -O /dev/null
+wget --quiet "https://acctest.acc.jlab.org/RFDashboard/ajax/cavity?end=`date +'%F'`&start=`date --date='1 week ago' +'%F'`&timeUnit=day" -O /dev/null
